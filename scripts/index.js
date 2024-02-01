f4a6a0d3dc6655f9057a9c67c0d32b467fcbc02e
Michael Owooje <michaelowooje@gmail.com>
2024-01-31 19:36:53 -0500

    new update

--- scripts/index.js
+++ scripts/index.js
@@ -24,3 +24,20 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg ",
   },
 ];
+
+/* Elements */
+const profileEditButton = document.querySelector("#profile__edit-button");
+const modalCloseButton = document.querySelector("#modal-close-button");
+const profileEditModal = document.querySelector("#profile-edit-modal");
+const profileName = document.querySelector("#profile-title");
+const profileEditForm = document.querySelector("#profile-edit-form");
+const profileDescription = document.querySelector("#profile-description");
+const editProfileModalName = document.querySelector(
+  "#edit-profile-modal-title"
+);
+const editProfileModalDescription = document.querySelector(
+  "#edit-profile-modal-description"
+);
+const cardListEl = document.querySelector("#cards-list");
+const cardTemplate =
+  document.querySelector("#cards-template").content.firstElementChild;
