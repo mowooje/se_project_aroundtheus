a3d22d1d6d58c6e792b6fd751dc44fb1bda2eaea
Michael Owooje <150976936+mowooje@users.noreply.github.com>
2024-09-08 00:04:30 -0400

    Revert "Project 9 pull 1"

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,8 +1,7 @@
 export default class UserInfo {
-  constructor({ profileTitle, profileDescription, avatarSelector }) {
+  constructor({ profileTitle, profileDescription }) {
     this._title = document.querySelector(profileTitle);
     this._description = document.querySelector(profileDescription);
-    this._avatar = document.querySelector(avatarSelector);
   }
 
   getUserInfo() {
@@ -16,9 +15,5 @@ export default class UserInfo {
   setUserInfo(data) {
     this._title.textContent = data.title;
     this._description.textContent = data.description;
-  }
-
-  setAvatar({ avatar }) {
-    this._avatar.src = avatar;
   }
 }
