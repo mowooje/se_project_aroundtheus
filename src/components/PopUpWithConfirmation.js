6c95a8f39ac937c47f5f2898108a991a7ef5dfea
Michael Owooje <michaelowooje@gmail.com>
2024-10-09 19:25:48 -0400

    changed avatar modal text to Inter

--- src/components/PopUpWithConfirmation.js
+++ src/components/PopUpWithConfirmation.js
@@ -19,7 +19,7 @@ export default class PopUpWithConfirmation extends PopUp {
     this._handleFormSubmit = action;
   }
 
-  setLoading(submit, loadingText = "Saving...") {
+  setLoading(submit, loadingText = "Deleting...") {
     if (submit) {
       this._submitButton.textContent = loadingText;
     } else {
